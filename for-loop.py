f47d3b0e60e4969b697890a9e7fc28ce8cadbd8f
Talhakhan0819 <talhaniaz569@gmail.com>
2026-02-16 10:09:50 +0500

    Add files via upload

---
+++ for-loop.py
@@ -0,0 +1,29 @@
+# name = "Talha"
+
+# for i in name:
+#     print(i)
+
+
+
+   # For List
+# colors = ["Red", "Green", "Black", "White", "Blue"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+
+
+# for k in range(5):
+#     print(k)
+
+# for k in range(5):
+#     print(k + 1)
+
+
+# for i in range(100, 1001):
+#     print(i)
+
+
+for i in range(10, 0, -1):
+    print(i)
